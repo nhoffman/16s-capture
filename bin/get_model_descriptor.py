28e829804e162c5164b1b8ee999db319c1fd60bc
Noah Hoffman <noah.hoffman@gmail.com>
2018-10-01 12:45:58 -0700

    clean up get_model_descriptor.py a little

--- bin/get_model_descriptor.py
+++ bin/get_model_descriptor.py
@@ -1,30 +1,45 @@
+#!/usr/bin/env python
+
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('stats_file', help="file with raxml tree stats")
-args = parser.parse_args()
+import sys
 
 
-dna = False
-sub_mat = None
-alpha = None
-freqs = []
-rates = []
+def main(arguments):
 
-with open(args.stats_file, 'r') as file:
-    for line in file:
-        if line.startswith("DataType:"):
-            dna = (line.strip().split()[1] == "DNA")
-        elif line.startswith("Substitution Matrix"):
-            sub_mat = line.strip().split()[2]
-            if not dna and sub_mat == "GTR":
-                sub_mat = "PROTGTR"
-        elif line.startswith("Base frequencies:"):
-            freqs = line.strip().split()[2:]
-        elif line.startswith("alpha"):
-            _, raw_alpha, raw_rates = line.strip().split(':')
-            alpha = raw_alpha.split()[0]
-            rates = raw_rates.split()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('stats_file', help="file with raxml tree stats")
+    args = parser.parse_args(arguments)
 
-model_descriptor = sub_mat + "{" + "/".join(rates) + "}+FU{" + "/".join(freqs) + "}+G4{" + alpha + "}"
-print(model_descriptor)
+    is_dna = False
+    sub_mat = None
+    alpha = None
+    freqs = []
+    rates = []
+
+    with open(args.stats_file, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith("DataType:"):
+                is_dna = 'DNA' in line
+            elif line.startswith("Substitution Matrix"):
+                sub_mat = line.split()[2]
+                if sub_mat == "GTR" and not is_dna:
+                    sub_mat = "PROTGTR"
+            elif line.startswith("Base frequencies:"):
+                freqs = line.split()[2:]
+            elif line.startswith("alpha"):
+                _, raw_alpha, raw_rates = line.split(':')
+                alpha = raw_alpha.split()[0]
+                rates = raw_rates.split()
+
+    model = '{sub_mat}{{{rates}}}+FU{{{freqs}}}+G4{{{alpha}}}'.format(
+        sub_mat=sub_mat,
+        rates='/'.join(rates),
+        freqs='/'.join(freqs),
+        alpha=alpha
+    )
+    print(model)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
